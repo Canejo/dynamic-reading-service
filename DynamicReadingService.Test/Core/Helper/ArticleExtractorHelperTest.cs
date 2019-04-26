45a301ab537a47068ded6bde8d55f8da1fab6677
Felipe Canejo <felipecanejo@gmail.com>
2019-04-25 22:37:53 -0300

    Test article extractor

---
+++ DynamicReadingService.Test/Core/Helper/ArticleExtractorHelperTest.cs
@@ -0,0 +1,20 @@
+﻿using DynamicReadingService.Core.Helper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicReadingService.Test.Core.Helper
+{
+    [TestClass]
+    public class ArticleExtractorHelperTest
+    {
+        [TestMethod]
+        public void ArticleExtractor_Success()
+        {
+            string url = "https://www.wuxiaworld.com/novel/lord-of-all-realms/chapter-1-an-unstable-situation";
+            string content = ArticleExtractorHelper.Run(url);
+            Assert.IsNotNull(content);
+        }
+    }
+}
