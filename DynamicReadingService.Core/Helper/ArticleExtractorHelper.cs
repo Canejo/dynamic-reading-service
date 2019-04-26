aa5406d216448c7871873b831b212ddac0cb0e37
Felipe Canejo <felipecanejo@gmail.com>
2019-04-25 22:30:24 -0300

    Create ArticleExtractorHelper.cs

---
+++ DynamicReadingService.Core/Helper/ArticleExtractorHelper.cs
@@ -0,0 +1,19 @@
+﻿using NReadability;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicReadingService.Core.Helper
+{
+    public static class ArticleExtractorHelper
+    {
+        public static string Run(string url)
+        {
+            var transcoder = new NReadabilityWebTranscoder();
+            var input = new WebTranscodingInput(url);
+
+            var result = transcoder.Transcode(input);
+            return result.ExtractedContent;
+        }
+    }
+}
